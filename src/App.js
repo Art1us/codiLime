a79943a3c0f0a940774fb32a46ae53a116ffdb07
Art1us <109896229+Art1us@users.noreply.github.com>
2022-10-09 00:14:59 +0200

    fix folder structure

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React,{ useState }  from "react";
-import './App.css'
-import Header from "./components/Header/Header";
-import HeroBanner from "./components/HeroBanner/HeroBanner";
-import MiddleSection from "./components/MiddleSection/MiddleSection";
-import QuoteSection from "./components/QuoteSection/QuoteSection";
-import BottomSection from "./components/BottomSection/BottomSection";
-import Subscribe from "./components/Subscribe/Subscribe";
-import Footer from "./components/Footer/Footer";
+import React, { useState } from "react";
+import "./App.css";
 
+import {
+  Header,
+  HeroBanner,
+  MiddleSection,
+  QuoteSection,
+  BottomSection,
+  Subscribe,
+  Footer,
+} from "./components";
 
 function App() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
 
-  const toggleTheme = () =>{
-    setTheme(prev => prev === 'light' ? 'dark' : 'light')
-  }
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
 
   return (
-      <div className="app" data-theme={theme}>
-        <Header toggleTheme={toggleTheme} theme={theme}/>
-        <HeroBanner />
-        <MiddleSection />
-        <QuoteSection />
-        <BottomSection />
-        <Subscribe />
-        <Footer />
-      </div>
+    <div className="app" data-theme={theme}>
+      <Header toggleTheme={toggleTheme} theme={theme} />
+      <HeroBanner />
+      <MiddleSection />
+      <QuoteSection />
+      <BottomSection />
+      <Subscribe />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Header } from "./Header/Header";
+export { default as HeroBanner } from "./HeroBanner/HeroBanner";
+export { default as MiddleSection } from "./MiddleSection/MiddleSection";
+export { default as QuoteSection } from "./QuoteSection/QuoteSection";
+export { default as BottomSection } from "./BottomSection/BottomSection";
+export { default as Subscribe } from "./Subscribe/Subscribe";
+export { default as Footer } from "./Footer/Footer";
